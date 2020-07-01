7d22627adcb713fad1cc8fd6aea7dc776157621e
wang <417787371@qq.com>
2020-07-01 16:46:10 +0800

    ...

--- routes/views.js
+++ routes/views.js
@@ -4,7 +4,6 @@ const path = require('path')
 router.prefix('/view')
 
 router.get('/', async (ctx, next) => {
-    console.log(ctx)
     await ctx.render('index', {
         title: '项目列表',
         serverPort: ctx.request.header.host.split(':')[1]
